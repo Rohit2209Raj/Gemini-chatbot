427592c6ae14fbf43ca654b4fcf52abd2bc4b8f1
Rohit Raj <rohitrajptn2209@gmail.com>
2026-06-14 17:58:14 +0530

    Remove direct access to chat

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 from fastapi import FastAPI,HTTPException
 from chat import chatting
 from dotenv import load_dotenv
+from verify import check_api_key
+from fastapi.responses import RedirectResponse
 import os
 
 
 app=FastAPI()
 load_dotenv()
 
+logged_in=False
+
 @app.get('/')
 def homepage():
     return {'message':'Welcome to my AI Gemini Chatbot'}
 
-
-@app.post('/chat')
-def chat_with_gemini(api_key:str,question:str)->str:
-    if api_key != os.getenv('USER_API_KEY'):
+@app.post('/login')
+def check_client(api_key:str):
+    global logged_in
+    if check_api_key(api_key) == True:
+        logged_in=True
+        RedirectResponse(
+            url="/chat",
+            status_code=303
+        )
+    else:
         raise HTTPException(
             status_code=401,
-            detail='Invalid api_key'
+            detail='Invalid API_KEY'
         )
+        
 
-    answer = chatting(question)
-    return (answer)
+
+@app.post('/chat')
+def chat_with_gemini(question:str)->str:
+    if not logged_in:
+        raise HTTPException(
+            status_code=403,
+            detail="Login required"
+        )
+    else:
+        answer = chatting(question)
+        return (answer)
 
 
