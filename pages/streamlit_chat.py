231fff3241a6eb2f6ab77975cb87ce20badffc85
Rohit Raj <rohitrajptn2209@gmail.com>
2026-06-16 09:32:44 +0530

    Multichat achieved

--- pages/streamlit_chat.py
+++ pages/streamlit_chat.py
@@ -13,15 +13,44 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-question=st.text_area("Enter your thoughts ",height=100)
+context=""
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
 
-if st.button("Answer"):
-    with st.spinner("Giving you the suitable answer"):
-        response=requests.post(
-                url='http://127.0.0.1:8000/chat',
-                params={'question':question}
-        )
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
 
-        # st.success(response.status_code)
-        st.success(response.json())  # See what actually came back
+if prompt := st.chat_input("Enter your thoughts"):
+
+    st.session_state.messages.append({'role':'user','content':prompt})
+    with st.chat_message('user'):
+        context+=prompt
+        st.markdown(context)
+    
+    response=requests.post(
+        url='http://127.0.0.1:8000/chat',
+        params={'question':context}
+    )
+
+    reply=response.json()
+
+    st.session_state.messages.append({'role':'assistant','content':reply})
+    with st.chat_message('assistant'):
+        st.markdown(reply)
+
+    
+
+
+# question=st.text_area("Enter your thoughts ",height=100)
+
+# if st.button("Answer"):
+#     with st.spinner("Giving you the suitable answer"):
+#         response=requests.post(
+#                 url='http://127.0.0.1:8000/chat',
+#                 params={'question':question}
+#         )
+
+#         # st.success(response.status_code)
+#         st.success(response.json())  # See what actually came back
 
